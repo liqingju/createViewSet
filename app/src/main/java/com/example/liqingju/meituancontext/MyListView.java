7c53c04a78ac12d230a1b4b14952ce14530984ec
liqingju <973683374@qq.com>
2016-01-08 00:11:51 +0800

    Revert "comin"
    This reverts commit cdf804c69e0a51e0f171c60e9753f4c6b2995016.

--- app/src/main/java/com/example/liqingju/meituancontext/MyListView.java
+++ app/src/main/java/com/example/liqingju/meituancontext/MyListView.java
@@ -2,42 +2,42 @@ package com.example.liqingju.meituancontext;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Interpolator;
 import android.widget.AbsListView;
-import android.widget.AbsListView.OnScrollListener;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.Scroller;
+import android.widget.TextView;
 
-public class MyListView extends ListView implements OnScrollListener {
+/**
+ * Created by liqingju on 15/12/14.
+ */
+public class MyListView extends ListView implements AbsListView.OnScrollListener {
     private Context mContext;
-    private int hight;
-    private int wight;
-    private int havaHight;
-    private FrameLayout frameLayout;
-    private ImageView mImageView;
-    private float mListY;
-    private ScalingRunnalable runAntion = new ScalingRunnalable();
-    private static final Interpolator sInterpolator = new Interpolator() {
-        public float getInterpolation(float paramAnonymousFloat) {
-            System.out.println("paramAnonymousFloat==  " + paramAnonymousFloat);
-            float f = paramAnonymousFloat - 1.0F;
-            return 1.0F + f * (f * (f * (f * f)));
-        }
-    };
+    private View headView;
+    private ImageView imageView;
+    private int lastY;
+    private FrameLayout mFramelayout;
+    private float allHight;
+    private int allWight;
+    private int haveHight;
+    private VelocityTracker mVlocity;
+   // ScrollView
+    TextView text;
 
-    // private float
 
     public MyListView(Context context) {
         super(context);
         initData(context);
-
     }
 
     public MyListView(Context context, AttributeSet attrs) {
@@ -52,70 +52,96 @@ public class MyListView extends ListView implements OnScrollListener {
 
     private void initData(Context context) {
         this.mContext = context;
+        mVlocity =VelocityTracker.obtain();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        allHight = displayMetrics.heightPixels;
+        allWight = displayMetrics.widthPixels;
+        mFramelayout = new FrameLayout(context);
+        mFramelayout.setBackgroundColor(0X000000);
+        haveHight = (int) (9f * (allWight / 16.0f));
+        Log.e("haveHight== ", haveHight + "  " + allHight);
+        LayoutParams layoutParams = new LayoutParams(allWight, haveHight);
+        mFramelayout.setLayoutParams(layoutParams);
+        FrameLayout.LayoutParams layoutParamsImageView = new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        imageView = new ImageView(context);
+        layoutParamsImageView.gravity = 80;
+//        imageView.setLayoutParams(layoutParamsImageView);
+        mFramelayout.addView(imageView);
 
-        DisplayMetrics metrics = new DisplayMetrics();
-        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        hight = metrics.heightPixels;
-        wight = metrics.widthPixels;
-        frameLayout = new FrameLayout(mContext);
-        havaHight = (int) (9f * (wight / 16.0f));
-        AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(wight, havaHight);
-        frameLayout.setLayoutParams(layoutParams);
-        mImageView = new ImageView(mContext);
-        frameLayout.addView(mImageView);
-        addHeaderView(frameLayout);
+//        LayoutInflater inflater = LayoutInflater.from(mContext);
+//        headView = inflater.inflate(R.layout.sample_my_view, null);
+//        imageView = (ImageView) headView.findViewById(R.id.hander_image);
+        addHeaderView(mFramelayout);
         super.setOnScrollListener(this);
+//     addHeaderView( );
+
 
     }
+
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
+
+
         switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN: // 点击
-                if (mListY == 0) {
-                    mListY = ev.getY();
+            case MotionEvent.ACTION_DOWN:
+                if (lastY == 0) {
+                    lastY = (int) ev.getY();
                 }
-
                 break;
-            case MotionEvent.ACTION_MOVE: // 移动
-                float Y = ev.getY();
-                int frameButtom = frameLayout.getBottom();
-                android.view.ViewGroup.LayoutParams layoutParams = frameLayout.getLayoutParams();
-                if (frameButtom >= havaHight && frameButtom <= (hight * 0.75)) {
+            case MotionEvent.ACTION_MOVE:
+                int Y = (int) ev.getY();
+                mVlocity.addMovement(ev);
+                mVlocity.computeCurrentVelocity(1000);
+                mVlocity.getYVelocity();
+                int hight = getFrameLayout().getBottom();
+                ViewGroup.LayoutParams layoutParams = mFramelayout.getLayoutParams();
+                if (hight >= haveHight) {
 
-                    if (frameButtom <= (hight * 0.75)) {
-                        layoutParams.height = frameLayout.getHeight() + (int) (Y - mListY);
-                        frameLayout.setLayoutParams(layoutParams);
-                        mListY = Y;
+                    if (hight <= allHight * 0.75f) {
+                        if (getFrameLayout().getHeight() + (Y - lastY) > allHight * 0.75f) {
+                            layoutParams.height = (int)(allHight * 0.75f);
+                        } else {
+                            layoutParams.height = getFrameLayout().getHeight() + (Y - lastY);
+                        }
+
+
+                        getFrameLayout().setLayoutParams(layoutParams);
+                        lastY = (int) ev.getY();
                         return true;
                     }
-                    if (frameButtom <= havaHight) {
-                        layoutParams.height = havaHight;
-                        frameLayout.setLayoutParams(layoutParams);
-                        mListY = Y;
+
+                    if (hight <haveHight) {
+                        Log.e("=====","走这样的额");
+                        layoutParams.height = this.haveHight;
+                        this.getFrameLayout().setLayoutParams(layoutParams);
                         return super.onTouchEvent(ev);
+
                     }
+                    return true;
 
                 }
-                if (frameButtom <= havaHight) {
-                    layoutParams.height = havaHight;
-                    frameLayout.setLayoutParams(layoutParams);
-                    mListY = Y;
-                    return super.onTouchEvent(ev);
+
+
+                lastY = (int) ev.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                if (getY()<0){
+                   scrollTo(0,0);
+                    getFrameLayout().scrollTo(0,0);
                 }
-
-                break;
-            case MotionEvent.ACTION_UP: // 离开
-                mListY = 0;
-                runAntion.startAnimation(200L);
+                mVlocity.clear();
+                Log.e("BOTTOM", getFrameLayout().getBottom() + "   " + getFrameLayout().getHeight() + "  " + getFrameLayout().getScrollY()+"  "+getY());
                 break;
 
-            default:
-                break;
+
         }
-        mListY = ev.getY();
+        lastY = (int) ev.getY();
+
         return super.onTouchEvent(ev);
     }
+
 
     @Override
     public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -124,65 +150,28 @@ public class MyListView extends ListView implements OnScrollListener {
 
     @Override
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-        float f = havaHight - frameLayout.getBottom();
-        if (f > 0.0 && f <= havaHight) {
-            int dex = (int) (0.65d * f);
-            mImageView.scrollTo(0, -dex);
-            System.out.println("走这里====  " + dex + "   " + f);
-        } else if (mImageView.getScrollY() != 0) {
-            mImageView.scrollTo(0, 0);
+        int f = haveHight - getFrameLayout().getBottom();
 
+        if (f > 0.0f && f < haveHight) {
+            double i = (0.65d * f);
+            Log.e("huadong", i + "");
+            imageView.scrollTo(0, -(int) i);
+        } else if (imageView.getScrollY() != 0) {
+            Log.e("imageView=11=  ", "" + imageView.getScrollY());
+            imageView.scrollTo(0, 0);
         }
 
+
+        Log.e("imageView==  ", "" + imageView.getScrollY());
     }
 
-    public ImageView getImageView() {
+    public ImageView getHanderImageView() {
 
-        return mImageView;
+        return imageView;
     }
 
-    class ScalingRunnalable implements Runnable {
-        long mDuration;
-        boolean mIsFinished = true;
-        float mScale;
-        long mStartTime;
+    public FrameLayout getFrameLayout() {
 
-        ScalingRunnalable() {
-        }
-
-        public void abortAnimation() {
-            this.mIsFinished = true;
-        }
-
-        public boolean isFinished() {
-            return this.mIsFinished;
-        }
-
-        public void run() {
-            float f2;
-            ViewGroup.LayoutParams localLayoutParams;
-            if ((!this.mIsFinished) && (this.mScale > 1.0D)) {
-                float f1 = ((float) SystemClock.currentThreadTimeMillis() - (float) this.mStartTime) / (float) this.mDuration;
-                f2 = this.mScale - (this.mScale - 1.0F) * sInterpolator.getInterpolation(f1);
-                localLayoutParams = frameLayout.getLayoutParams();
-                if (f2 > 1.0F) {
-                    Log.d("mmm", "f2>1.0");
-                    localLayoutParams.height = ((int) (f2 * havaHight));
-                    frameLayout.setLayoutParams(localLayoutParams);
-                    post(this);
-                    return;
-                }
-                this.mIsFinished = true;
-            }
-        }
-
-        public void startAnimation(long paramLong) {
-            this.mStartTime = SystemClock.currentThreadTimeMillis();
-            this.mDuration = paramLong;
-            this.mScale = ((float) (frameLayout.getBottom()) / havaHight);
-            this.mIsFinished = false;
-            post(this);
-        }
+        return mFramelayout;
     }
-
 }
