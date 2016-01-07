f6bba836070d77da196180da308ee918c3a33b27
liqingju <973683374@qq.com>
2016-01-08 00:34:40 +0800

    commint
    回滚到上个版本

---
+++ app/src/main/java/com/example/liqingju/meituancontext/ScrollViewActivity.java
@@ -0,0 +1,18 @@
+package com.example.liqingju.meituancontext;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+/**
+ * Created by liqingju on 16/1/7.
+ */
+public class ScrollViewActivity extends Activity {
+//    LinearLayout
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scrollviewactivity);
+    }
+}
